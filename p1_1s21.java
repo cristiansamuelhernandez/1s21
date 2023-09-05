a0faebf426eca3dcfc7b7befc1267764ab026baf
cristiansamuelhernandez <144179323+cristiansamuelhernandez@users.noreply.github.com>
2023-09-05 16:07:39 -0600

    cristian samuel hernandez trejo
    practica 1 practica 2

---
+++ p1_1s21.java
@@ -0,0 +1,3 @@
+class p1_1s21{
+public static void main(String[]args)
+   System.out.print("este es mi primer programa")
---
+++ p2_1s21.java
@@ -0,0 +1,12 @@
+class p2_1s21{
+	public static vold main(String[] args) {
+		System.out.println("Datos personales");
+		Nombre.println("cristian samuel hernandez trejo")
+		Edad.println("21")
+		Correo.println("tiancristc@gmail.Com")
+		Aficiones.("nadar,correr y hacer ejercicio") 
+		Color.println("Azul")
+
+     }
+}
+
