25a0225412f92cb4a813fd563c94ad964d1cece9
cristiansamuelhernandez <144179323+cristiansamuelhernandez@users.noreply.github.com>
2023-09-09 15:37:15 -0600

    Add files via upload

---
+++ p4_1s21.java
@@ -0,0 +1,33 @@
+
+ import java.util.Scanner;
+
+  class p4_1s21{
+  	public  static void main(String[] args)
+  	Scanner teclado = new Scanner(System.in);
+  	String nombre;
+  	System.out.println("Ingresa tu nombre")
+  	nombre = teclado.nextLine();
+
+
+  	Scanner anos = new Scanner(System.in);
+  	String edad;
+  	System.out.println("Ingresa tu edad");
+  	edad = teclado.nextLine();
+
+
+  	Scanner emil = new Scanner(System.in);
+  	String correo;
+  	System.out.printlen("Ingresa tu correo");
+  	correo = teclado.nextLine();
+
+
+  	Scanner celular = new Scaner(System.in);
+  	String corrreo;
+  	System.out.println("Ingresa tu telefoono");
+  	telefono = teclado.nextLine();
+  	System.out.println("Bienbenido" + nombre);
+  	System.out.println("Tu edad es" + edad);
+  	System.out.println("Tu correo es" + correo);
+  	System.out.println("Tu nombre es" + telefono)
+  	 }
+  	    
