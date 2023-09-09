e35e14d2ba9dc3204dca93db63cd78a716f2558a
cristiansamuelhernandez <144179323+cristiansamuelhernandez@users.noreply.github.com>
2023-09-08 20:32:47 -0600

    Add files via upload

---
+++ p3_1s21.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class p3_1s21{
+	public static void main(String[] args) {
+		Scanner teclado = new Scanner(System.in);
+		String cadena;
+		System.out.println("ingresa tu Nombre");
+		cadena = teclado.nextLine();
+		System.out.println("Bienvenido" + cadena + ", a la clase");
+    }
+}
